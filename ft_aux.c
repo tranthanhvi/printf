8a03a1605780c0e5f6234b9d42a19feab3f4a472
Thanh Tran <tranxanhvi@gmail.com>
2024-11-25 23:42:04 +0200

    finished ft_aux.c

--- ft_aux.c
+++ ft_aux.c
@@ -69,3 +69,20 @@ static size_t ft_len(unsigned long long num, char *base)
 }
 
 char	*ft_aux(unsigned long long num, char *base)
+{
+	char	*str;
+	int		num_len;
+	int		base_len;
+	num_len = ft_len(num, base);
+	base_len = ft_strlen(base);
+	str = ft_calloc((num_len + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	while (num_len)
+	{
+		num_len--;
+		str[num_len] = base[num % base_len];
+		num /= base_len;
+	}
+	return (str);
+}
