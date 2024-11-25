352d757db964175701393ea82b122ee545daa622
Thanh Tran <tranxanhvi@gmail.com>
2024-11-25 20:44:35 +0200

    added print_char.c

--- ft_print_char.c
+++ ft_print_char.c
@@ -10,3 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+void	ft_print_char(char c, size_t *counter)
+{
+	write(1, &c, 1);
+	(*counter)++;
+}
--- ft_printf.h
+++ ft_printf.h
@@ -1,19 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: thantran <thantran@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 23:12:10 by thantran          #+#    #+#             */
-/*   Updated: 2024/11/21 23:12:10 by thantran         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
 # include <stdarg.h>
-# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define HEX_UPP_BASE "0123456789ABCDEF"
+# define HEX_LOW_BASE "0123456789abcdef"
+
+int		ft_printf(char const *str, ...);
+
+/* format functions */
+void	ft_print_char(char c, size_t *counter);
+void	ft_print_str(char *str, size_t *counter);
+void	ft_print_int(int num, size_t *counter);
+void	ft_print_uint(unsigned int num, size_t *counter);
+void	ft_print_hex(unsigned int num, size_t *counter, char *base);
+void	ft_print_ptr(void *ptr, size_t *counter);
+
+/* auxiliary functions */
+char	*ft_aux_pf(unsigned long long n, char *base);
 
 #endif
