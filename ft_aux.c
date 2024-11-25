ec27edfb1a51a9fc8828e736f379d2a9856875b0
Thanh Tran <tranxanhvi@gmail.com>
2024-11-25 22:59:19 +0200

    added helper functions

--- ft_aux.c
+++ ft_aux.c
@@ -12,3 +12,26 @@
 
 #include "ft_printf.h"
 
+size_t	ft_strlen(const	char *str)
+{
+	size_t	count;
+	
+	if (!str)
+		return (0);
+	count = 0;
+	while (str[count])
+		count++;
+	return (count)
+}
+
+void	ft_bzero(void *pointer, size_t size)
+{
+	unsigned char	*ptr;
+
+	p = (unsigned char *)pointer;
+	while (size > 0)
+	{
+		*ptr++ = 0;
+		size--;
+	}
+}
